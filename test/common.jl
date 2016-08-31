0e5e031a19ca2a1db261e81f86a146556c4f98b0
sunoru sidw <s@sunoru.com>
2016-08-31 20:15:28 +0800

    Test fixed.

--- test/common.jl
+++ test/common.jl
@@ -5,3 +5,14 @@ brng_types = [
     VSL_BRNG_MCG31, VSL_BRNG_R250, VSL_BRNG_MRG32K3A, VSL_BRNG_MCG59, VSL_BRNG_WH, VSL_BRNG_SOBOL, VSL_BRNG_NIEDERR,
     VSL_BRNG_MT19937, VSL_BRNG_MT2203, VSL_BRNG_SFMT19937, VSL_BRNG_NONDETERM, VSL_BRNG_ARS5, VSL_BRNG_PHILOX4X32X10
 ]
+
+macro test_rand(expr)
+    values = @eval $expr
+    if isa(values, Array)
+        for value in values
+            @printf "%.6f\n" value
+        end
+    else
+        @printf "%.6f\n" values
+    end
+end
