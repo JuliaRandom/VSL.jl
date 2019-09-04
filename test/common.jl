6db36d0706ad3ea01897397649388635bbc276e2
sunoru <s@sunoru.com>
2019-09-04 16:34:59 -0400

    Add `Printf` for test.

--- test/common.jl
+++ test/common.jl
@@ -1,5 +1,6 @@
 using VSL
 import Test: @test
+import Printf: @printf
 
 brng_types = [
     VSL_BRNG_MCG31, VSL_BRNG_R250, VSL_BRNG_MRG32K3A, VSL_BRNG_MCG59, VSL_BRNG_WH, VSL_BRNG_SOBOL, VSL_BRNG_NIEDERR,
